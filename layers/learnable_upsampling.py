fix bug: KL is negitive, should be positive
root cause:
z_p and z_p is after quantizer, should be before quantizer

layers/learnable_upsampling.py
@@ -45,17 +45,9 @@ class LearnableUpsampling(nn.Module):
 
         self.proj_o = LinearNorm(192, 192 * 2)
 
-    def forward(self, duration, tokens, src_len, src_mask, tgt_len, max_src_len):
+    def forward(self, duration, tokens, src_len, src_mask, max_src_len):
 
         batch_size = duration.shape[0]
-
-        # Duration Interpretation
-        # if tgt_len is None:
-        #     mel_len = torch.round(duration.sum(-1)).type(torch.LongTensor).to(tokens.device)
-        # else:
-        #     # fix: mel_len should be the target_lens(y_lengths)
-        #     mel_len = torch.round(tgt_len.type(torch.LongTensor).to(tokens.device))
-
         mel_len = torch.round(duration.sum(-1)).type(torch.LongTensor).to(tokens.device)
 
         mel_len = torch.clamp(mel_len, max=self.max_seq_len)

layers/losses.py
@@ -200,20 +200,20 @@ class NaturalSpeechGeneratorLoss(nn.Module):
         logs_p = logs_p.float()
         z_mask = z_mask.float()
 
-        # pad if size is not equal.
-        # distribution of logs_p and logs_q may different, make them the same size
-        if logs_p.size(2) != logs_q.size(2):
-            length = max(logs_p.size(2), logs_q.size(2))
-            if z_p.size(2) != length:
-                z_p = F.pad(z_p, (0, length - z_p.size(2)), "constant", 0)
-            if logs_p.size(2) != length:
-                logs_p = F.pad(logs_p, (0, length - logs_p.size(2)), "constant", 0)
-            if m_p.size(2) != length:
-                m_p = F.pad(m_p, (0, length - m_p.size(2)), "constant", 0)
-            if logs_q.size(2) != length:
-                logs_q = F.pad(logs_q, (0, length - logs_q.size(2)), "constant", 0)
-            if z_mask.size(2) != length:
-                z_mask = F.pad(z_mask, (0, length - z_mask.size(2)), "constant", 0)
+        # # pad if size is not equal.
+        # # distribution of logs_p and logs_q may different, make them the same size
+        # if logs_p.size(2) != logs_q.size(2):
+        #     length = max(logs_p.size(2), logs_q.size(2))
+        #     if z_p.size(2) != length:
+        #         z_p = F.pad(z_p, (0, length - z_p.size(2)), "constant", 0)
+        #     if logs_p.size(2) != length:
+        #         logs_p = F.pad(logs_p, (0, length - logs_p.size(2)), "constant", 0)
+        #     if m_p.size(2) != length:
+        #         m_p = F.pad(m_p, (0, length - m_p.size(2)), "constant", 0)
+        #     if logs_q.size(2) != length:
+        #         logs_q = F.pad(logs_q, (0, length - logs_q.size(2)), "constant", 0)
+        #     if z_mask.size(2) != length:
+        #         z_mask = F.pad(z_mask, (0, length - z_mask.size(2)), "constant", 0)
 
         # kl_loss 的介绍在视频 https://www.bilibili.com/video/BV1VG411h75N/?spm_id_from=333.788&vd_source=d38c9d5cf896f215d746bb79474d6606
         # 的 1:38:43 开始处

recipes/naturaltts/naturaltts.py
@@ -139,6 +139,9 @@ class NaturalTTSModel(nn.Module):
             self.embedded_language_dim = 0
 
     def match_mel_token(self, z_p, m_p, logs_p, x_mask, y_mask):
+        """
+        match the mel and token to get the duration. duration in the source dataset is not used because it may be wrong.
+        """
         # find the alignment path
         attn_mask = torch.unsqueeze(x_mask, -1) * torch.unsqueeze(y_mask, 2)
         with torch.no_grad():
@@ -187,10 +190,10 @@ class NaturalTTSModel(nn.Module):
         z_p = self.flow(z, z_mask, g=None)
 
         # quantize z using RVQ
-        z, _, _ = self.quantizer(z)
+        z_quant, _, _ = self.quantizer(z)
 
         z_slice, ids_slice = rand_segments(
-            x=z,
+            x=z_quant,
             x_lengths=y_lengths,
             segment_size=self.spec_segment_size,
             let_short_samples=True,
@@ -221,7 +224,6 @@ class NaturalTTSModel(nn.Module):
             tokens=x.transpose(1, 2),
             src_len=x_lengths,
             src_mask=~(x_mask.squeeze(1).bool()),
-            tgt_len=y_lengths,
             max_src_len=x_lengths.max(),
         )
         p_mask = p_mask.unsqueeze(1)
@@ -247,13 +249,15 @@ class NaturalTTSModel(nn.Module):
             g=None,
             reverse=True
         )
+        # z_p should minus pitch_embed before compute KL loss with (m_p, logs_p)
+        z_p = z_p - pitch_embed
 
         # quantize z using RVQ
-        z_q, _, _ = self.quantizer(z_q)
+        z_q_quant, _, _ = self.quantizer(z_q)
 
         z_q_lengths = p_mask.flatten(1, -1).sum(dim=-1).long()
         z_slice_q, ids_slice_q = rand_segments(
-            x=z_q,
+            x=z_q_quant,
             x_lengths=torch.minimum(z_q_lengths, y_lengths),
             segment_size=self.spec_segment_size,
             let_short_samples=True,
@@ -361,9 +365,6 @@ class NaturalTTSTrain(TrainerModelWithDataset):
 
     def train_step(self, batch: Dict, criterion: nn.Module, optimizer_idx: int) -> Tuple[Dict, Dict]:
         spec_lens = batch["spec_lens"]
-
-        print(torch.cuda.memory_allocated())
-
         if optimizer_idx == 0:
             # print(batch["raw_texts"])
 
@@ -374,8 +375,6 @@ class NaturalTTSTrain(TrainerModelWithDataset):
             wav = batch["waveform"]
             pitch = batch["pitch"]
 
-            print(torch.cuda.memory_allocated())
-
             # generator pass
             outputs = self.generator(
                 x=tokens,
@@ -384,7 +383,6 @@ class NaturalTTSTrain(TrainerModelWithDataset):
                 y_lengths=spec_lens,
                 pitch=pitch
             )
-            print(torch.cuda.memory_allocated())
 
             wav_slice = segment(
                 x=wav,