after fixing kl_forward is negative, now KL is negative again. fix it.
root cause:
pitch is in shape of audio spec and add to x after upsampling. it affects the KL
change pitch estimation to shape of text
and also change the ground true pitch to shape of text for calculating the pitch loss

preprocess/gen_audio_stat.py
@@ -23,9 +23,9 @@ from util.audio_processor import AudioProcessor
 # generate audio pitch, duration and save to file
 ###
 
-def get_text(text, hps):
-    text_norm = text_to_sequence(text, hps.data.text_cleaners)
-    if hps.data.add_blank:
+def get_text(text, config):
+    text_norm = text_to_sequence(text, config.data.text_cleaners)
+    if config.data.add_blank:
         text_norm = commons.intersperse(text_norm, 0)
     text_norm = torch.LongTensor(text_norm)
     return text_norm
@@ -33,44 +33,45 @@ def get_text(text, hps):
 def gen_vits_model():
     current_path = os.path.dirname(os.path.abspath(__file__))
     os.chdir(os.path.join(current_path, "reference/vits"))  # change the path to vits path
-    hps = utils.get_hparams_from_file("./configs/vctk_base.json")
+    config = utils.get_hparams_from_file("./configs/vctk_base.json")
 
-    net_g = SynthesizerTrn(
+    vits_model = SynthesizerTrn(
         len(symbols),
-        hps.data.filter_length // 2 + 1,
-        hps.train.segment_size // hps.data.hop_length,
-        n_speakers=hps.data.n_speakers,
-        **hps.model).cuda()
-    _ = net_g.eval()
+        config.data.filter_length // 2 + 1,
+        config.train.segment_size // config.data.hop_length,
+        n_speakers=config.data.n_speakers,
+        **config.model
+    ).cuda()
+    _ = vits_model.train()
 
     print("loading pretrained checkpoint")
-    _ = utils.load_checkpoint("../vits_pretrained_vctk.pth", net_g, None)
+    _ = utils.load_checkpoint("../vits_pretrained_vctk.pth", vits_model, None)
 
     os.chdir(current_path)  # change the path back
-    return net_g, hps
+    return vits_model, config
 
-def gen_duration_using_vits(vits_model:torch.nn.Module, hps, text:str, sid=4):
+#TODO: maybe it's broken already. the sampling_rate of vits is different from naturaltts
+def gen_duration_using_vits(vits_model:torch.nn.Module, config, text:str, spec:torch.Tensor, sid=4):
     current_path = os.path.dirname(os.path.abspath(__file__))
     os.chdir(os.path.join(current_path, "reference/vits"))  # change the path to vits path
 
-    x_text = get_text(text, hps)
+    x_text = get_text(text, config)
     with torch.no_grad():
         x_text = x_text.cuda().unsqueeze(0)
         x_text_lengths = torch.LongTensor([x_text.size(1)]).cuda()
+        y_spec = spec.cuda().unsqueeze(0)
+        y_spec_lengths = torch.LongTensor([y_spec.size(2)]).cuda()
         sid = torch.LongTensor([sid]).cuda()
-        audio, attn, _, _ = vits_model.infer(
+
+        audio, _, attn, _, _, _, _ = vits_model.forward(
             x=x_text,
             x_lengths=x_text_lengths,
-            sid=sid,
-            noise_scale=.667,
-            noise_scale_w=0.8,
-            length_scale=1
+            y=y_spec,
+            y_lengths=y_spec_lengths,
+            sid=sid
         )
 
-        audio = audio[0, 0].data.cpu().float().numpy()
-        duration = attn.sum(2).flatten()
-        duration = duration.cpu().int().numpy()
-
+    duration = attn.sum(2)
     os.chdir(current_path)  #change the path back
     return audio, duration
 
@@ -111,15 +112,17 @@ def main(args):
         path = sample["audio"]
         pklpath = path + ".pkl"
         text = sample["text"]
-        if not args.refresh and os.path.exists(pklpath):
-            continue
+        # if not args.refresh and os.path.exists(pklpath):
+        #     continue
 
         # Load audio at the correct sample rate
         wav = processor.load_wav(path, sr=config.audio.sample_rate)
         audio = torch.from_numpy(wav)
         audio = audio.unsqueeze(0)
+        spec = processor.spectrogram(wav)
+        spec = torch.FloatTensor(spec)
 
-        _, duration = gen_duration_using_vits(vits, vits_config, text, sid=77)
+        _, duration = gen_duration_using_vits(vits, vits_config, text, spec, sid=77)
 
         # Infer pitch and periodicity
         pitch, periodicity = penn.from_audio(
@@ -162,6 +165,27 @@ def main(args):
         with open(pklpath, "wb") as fp:
             pickle.dump(obj=obj, file=fp )
 
+def gen_text_pitch(args):
+    config = VitsConfig()
+    config.load_json(args.config)
+    dataset_config = config.dataset_config
+
+    train_samples = get_metas_from_filelist(dataset_config.meta_file_train)
+    test_samples = get_metas_from_filelist(dataset_config.meta_file_val)
+    samples = train_samples
+    samples.extend(test_samples)
+
+    for sample in tqdm(samples):
+        path = sample["audio"]
+        pklpath = path + ".pkl"
+        text = sample["text"]
+        if not os.path.exists(pklpath): continue
+
+
+        with open(pklpath, "rb") as fp:
+            obj = pickle.load(fp)
+            duration = obj['duration']
+            pitch = obj['pitch']
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -172,6 +196,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args)
+    # gen_text_pitch(args)
 
     # pklpath = 'D:\\dataset\\VCTK\\wav48_silence_trimmed\\p226\\p226_089_mic1.flac.pkl'
     # with open(pklpath, "rb") as fp:

preprocess/reference/vits/models.py
@@ -7,7 +7,7 @@ from torch.nn import functional as F
 import commons
 import modules
 import attentions
-import monotonic_align
+from .monotonic_align import maximum_path
 
 from torch.nn import Conv1d, ConvTranspose1d, AvgPool1d, Conv2d
 from torch.nn.utils import weight_norm, remove_weight_norm, spectral_norm
@@ -477,7 +477,7 @@ class SynthesizerTrn(nn.Module):
       neg_cent = neg_cent1 + neg_cent2 + neg_cent3 + neg_cent4
 
       attn_mask = torch.unsqueeze(x_mask, 2) * torch.unsqueeze(y_mask, -1)
-      attn = monotonic_align.maximum_path(neg_cent, attn_mask.squeeze(1)).unsqueeze(1).detach()
+      attn = maximum_path(neg_cent, attn_mask.squeeze(1)).unsqueeze(1).detach()
 
     w = attn.sum(2)
     if self.use_sdp:

recipes/naturaltts/naturaltts.py
@@ -156,8 +156,8 @@ class NaturalTTSModel(nn.Module):
             attn = maximum_path(logp, attn_mask.squeeze(1)).unsqueeze(1).detach()  # [b, 1, t, t']
 
         # duration predictor
-        attn_durations = attn.sum(3)
-        return attn_durations
+        durations = attn.sum(3)
+        return durations, attn
 
     def forward(
         self,
@@ -219,9 +219,24 @@ class NaturalTTSModel(nn.Module):
             speech_prompts=prompts
         )  # duration_logw:[B,1,T]
         duration_p = torch.exp(duration_logw.unsqueeze(1)) * x_mask
-        duration_gt = self.match_mel_token(z_p, m_p, logs_p, x_mask, z_mask)
+        duration_gt, attn = self.match_mel_token(z_p, m_p, logs_p, x_mask, z_mask)
         duration_logw_gt = torch.log(duration_gt.unsqueeze(1) + 1e-6) * x_mask
         duration_loss = torch.sum((duration_logw - duration_logw_gt) ** 2, [1, 2]) / torch.sum(x_mask)
+
+        pitch_logw, pitch_embed = self.pitch_predictor(
+            x=x,
+            masks=x_mask,
+            speech_prompts=prompts
+        )  # pitch_logw:[B,1,T]
+        x = x + pitch_embed
+
+        pitch_p = torch.exp(pitch_logw.unsqueeze(1)) * x_mask
+        # map ground true pitch to the same dimention of x
+        pitch = torch.einsum("bcxy,bcy->bcx", attn, pitch.unsqueeze(1))
+        pitch = (pitch / (attn.sum(3) + 1e-6) * x_mask).round(decimals=3)
+        # compute pitch loss
+        pitch_logw_gt = torch.log(pitch.unsqueeze(1) + 1e-6) * x_mask
+        pitch_loss = torch.sum((pitch_logw - pitch_logw_gt) ** 2, [1, 2]) / torch.sum(x_mask)
 
         # differentiable durator (learnable upsampling)
         upsampled_rep, p_mask, _, W, C = self.learnable_upsampling(
@@ -234,22 +249,6 @@ class NaturalTTSModel(nn.Module):
         p_mask = p_mask.unsqueeze(1)
         m_p, logs_p = torch.split(upsampled_rep.transpose(1, 2), 192, dim=1)
         x_p = (m_p + torch.randn_like(m_p) * torch.exp(logs_p)) * p_mask
-
-        pitch_logw, pitch_embed = self.pitch_predictor(
-            x=x_p,
-            masks=p_mask,
-            speech_prompts=prompts
-        )  # pitch_logw:[B,1,T]
-
-        # compute pitch loss
-        pitch_p = torch.exp(pitch_logw.unsqueeze(1)) * z_mask
-        pitch_logw_gt = torch.log(pitch.unsqueeze(1) + 1e-6) * p_mask
-        pitch_loss = torch.sum((pitch_logw - pitch_logw_gt) ** 2, [1, 2]) / torch.sum(p_mask)
-
-        # add pitch_embed to embed pitch info into x_p, and z_p should minus pitch_embed.
-        # flow + pitch is the actual f(x), not the flow. so z_p should minus pitch_embed.
-        x_p = x_p + pitch_embed
-        z_p = z_p - pitch_embed
 
         z_q = self.flow(
             x=x_p,