7ffb592dbccd407959a91f340bae40b0bb3f3feb
cano <xch_554@163.com>
2024-03-12 22:40:15 +0800

    add decorator get_time and retry

--- util/decorator.py
+++ util/decorator.py
@@ -1,3 +1,6 @@
+from functools import wraps
+from typing import Callable, Any
+from time import sleep, perf_counter
 
 def exception_handler(func):
     def wrapper(*args, **kwargs):
@@ -8,4 +11,57 @@ def exception_handler(func):
             print(f"捕获到异常：{e}")
             # 可以返回一个默认值或者重新抛出异常
             return None
+    return wrapper
+
+
+def retry(retries: int = 3, delay: float = 1) -> Callable:
+    """
+    Attempt to call a function, if it fails, try again with a specified delay.
+
+    :param retries: The max amount of retries you want for the function call
+    :param delay: The delay (in seconds) between each function retry
+    :return:
+    """
+
+    # Don't let the user use this decorator if they are high
+    if retries < 1 or delay <= 0:
+        raise ValueError('Are you high, mate?')
+
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> Any:
+            for i in range(1, retries + 1):  # 1 to retries + 1 since upper bound is exclusive
+
+                try:
+                    print(f'Running ({i}): {func.__name__}()')
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    # Break out of the loop if the max amount of retries is exceeded
+                    if i == retries:
+                        print(f'Error: {repr(e)}.')
+                        print(f'"{func.__name__}()" failed after {retries} retries.')
+                        break
+                    else:
+                        print(f'Error: {repr(e)} -> Retrying...')
+                        sleep(delay)  # Add a delay before running the next iteration
+
+        return wrapper
+
+    return decorator
+
+
+def get_time(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+
+        # Note that timing your code once isn't the most reliable option
+        # for timing your code. Look into the timeit module for more accurate
+        # timing.
+        start_time: float = perf_counter()
+        result: Any = func(*args, **kwargs)
+        end_time: float = perf_counter()
+
+        print(f'"{func.__name__}()" took {end_time - start_time:.3f} seconds to execute')
+        return result
+
     return wrapper
